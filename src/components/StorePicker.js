c903b9a2188fe560514639b2421bb0bd0abfa509
Luke Joliat <lukejoliat@dswinc.com>
2018-07-11 14:55:51 -0400

    Update central components

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 export default class StorePicker extends React.Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import StorePicker from './components/StorePicker.js';
 import App from './components/App.js';
 import './css/style.css';
 
